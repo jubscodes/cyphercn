0dbfb1aedac52b014cd560841b71ae0293182fde
hoffms <jhoffmannburatto@gmail.com>
2026-02-25 01:14:54 -0300

    fix(cypher): resizable for react-resizable-panels v4 API
    - Use Group, Panel, Separator (replace PanelGroup, PanelResizeHandle)
    - Align cypher resizable with base component and v4 exports

--- components/ui/cypher/resizable.tsx
+++ components/ui/cypher/resizable.tsx
@@ -8,9 +8,9 @@ const ResizablePanelGroup = ({
   className,
   children,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
+}: React.ComponentProps<typeof ResizablePrimitive.Group>) => (
   <div className={cn("w-full h-full relative", className)}>
-    <ResizablePrimitive.PanelGroup
+    <ResizablePrimitive.Group
       className={cn(
         "flex h-full w-full data-[panel-group-direction=vertical]:flex-col"
       )}
@@ -23,7 +23,7 @@ const ResizablePanelGroup = ({
       <div className="absolute bottom-1 -left-1 w-1.5 h-1/2 bg-foreground dark:bg-ring pointer-events-none" />
       <div className="absolute top-1 -right-1 w-1.5 h-1/2 bg-foreground dark:bg-ring pointer-events-none" />
       <div className="absolute bottom-1 -right-1 w-1.5 h-1/2 bg-foreground dark:bg-ring pointer-events-none" />
-    </ResizablePrimitive.PanelGroup>
+    </ResizablePrimitive.Group>
   </div>
 );
 
@@ -43,10 +43,10 @@ const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+}: React.ComponentProps<typeof ResizablePrimitive.Separator> & {
   withHandle?: boolean;
 }) => (
-  <ResizablePrimitive.PanelResizeHandle
+  <ResizablePrimitive.Separator
     className={cn(
       `relative flex w-[1px] items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-[6px] data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90 focus:dark:outline-ring`,
       "bg-foreground dark:bg-ring",
@@ -58,7 +58,7 @@ const ResizableHandle = ({
     {withHandle && (
       <div className="relative w-[12px] aspect-video h-[20px] z-10 border-3 border-foreground dark:border-ring"></div>
     )}
-  </ResizablePrimitive.PanelResizeHandle>
+  </ResizablePrimitive.Separator>
 );
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle };
